69e07f86a82e6694196ee9bc3a18b78da7017dc5
Jasson Casey <jasson.casey@gmail.com>
2012-01-24 00:47:41 -0600

    Some crap ...

--- test.hs
+++ test.hs
@@ -1,11 +1,18 @@
 import System
+--import IO
 
 apply f [] = return ()
 apply f ( h : tl ) = do f h
                         apply f tl
 
-readFile f = do   handle <- (openFile f ReadMode)
-                  hClose handle
+fileToBuff f = readFile f
 
-main = do   x <- getArgs 
-            apply putStrLn x
+parse_file ( file, contents ) = putStrLn contents
+
+parse [] = return ()
+parse ( file : files ) = do   parse_file file
+                              parse files 
+
+loadFile file = ( file, readFile file )
+
+main = map loadFile getArgs
