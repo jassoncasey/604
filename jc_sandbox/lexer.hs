b00c000d5622f2070d8b8ec0c0e4684bfff7b65b
Jasson Casey <jasson.casey@gmail.com>
2012-01-24 16:33:54 -0600

    Rounding it out.

--- jc_sandbox/lexer.hs
+++ jc_sandbox/lexer.hs
@@ -5,39 +5,16 @@ import Char as Char
 data TokId = LetTok | EqTok | LamdaTok | DotTok | NatTok | IdTok | SemiTok
             | LParenTok | RParenTok | PlusTok | MinusTok | MultTok | DivTok
             deriving (Show)
+
 -- structure: string, line #, column #, filename
 data Lexeme = Lex String Int Int String deriving (Show)
+
 -- structure: type, lexeme
 data Token = Tok TokId Lexeme deriving (Show)
 
+-- simple token length accessor
+tokenSize :: Token -> Int
 tokenSize (Tok _ ( Lex str _ _ _ ) ) = length str
-
--- simple [0-9] character test
-isDigit :: Char -> Bool
-isDigit x = Char.isDigit x
-
-digitCls    = ['0'..'9']
-lalphaCls   = ['a'..'z']
-ualphaCls   = ['A'..'Z']
-alphaCls    = lalphaCls ++ ualphaCls
-alphaNumCls = digitCls ++ alphaCls
-firstIdCls  = alphaCls ++ ['_']
---IdCls       = firstIdCls ++ digitCls
-
--- simple [a-zA-Z_] character test
-isAlphaId :: Char -> Bool
-isAlphaId '_' = True
-isAlphaId x = isAlpha x
-
--- simple [a-zA-Z_0-9] character test
-isAlphaNumId :: Char -> Bool
-isAlphaNumId '_' = True
-isAlphaNumId x = Char.isAlphaNum x
-
--- simple head of line whitespace trim
-trim :: String -> String
-trim [] = []
-trim (h:tl) = if Char.isSpace h then trim tl else h:tl
 
 -- trim the head of the line that matches a character class
 trimPatternImpl :: String -> String -> String -> ( String, String )
@@ -53,6 +30,7 @@ trimPattern line pattern = trimPatternImpl [] line pattern
 
 -- simple single character operators, keywords, separators
 singleChar = [ '(', ')', '.', ';', '\\', '+', '-', '*', '/']
+isSingle :: Char -> Bool
 isSingle x = elem x singleChar
 
 -- simple function to create a single character token
@@ -71,11 +49,28 @@ tokenizeSingle ch lineno colno fname =
       '/'   -> Tok DivTok lex
       where lex = Lex [ch] lineno colno fname
 
-tokenize fname lineno colno (h:tl) 
-   | isSpace h = tokenize fname lineno colno+1 tl
+-- tokenize the leading consecutive decimal digits of a string
+tokenizeDigits :: String -> Int -> Int -> String -> Token
+tokenizeDigits line lineno colno fname = 
+   let result = trimPattern line ['0'..'9']
+   in Tok NatTok (Lex (fst result) lineno colno fname)
+
+-- tokneize a string into its possible compoents
+tokenizeImp :: String -> Int -> Int -> String -> [Token]
+tokenizeImp fname lineno colno (h:tl) 
+   -- remove whitespace
+   | isSpace h = tokenizeImp fname lineno (colno+1) tl
+   -- handle single character keywords and seperators
    | isSingle h = ( tokenizeSingle h lineno colno fname : 
-                     tokenize fname lineno (colno+1) tl )
-   | isDigit h = let tok = tokenizeDigit (h:tl) lineno colno fname 
-                     colno' = colno + (tokenSize tok)
-                  in result : ( tokenize fname lineno colno' )
+                     tokenizeImp fname lineno (colno+1) tl )
+   -- handle natural numbers
+   | Char.isDigit h =   let tok = tokenizeDigits (h:tl) lineno colno fname 
+                            tok_size = tokenSize tok
+                            colno' = colno + tok_size
+                        in tok : ( tokenizeImp fname lineno 
+                                       colno' ( drop tok_size (h:tl) ) )
    | otherwise = [] 
+
+-- tokenize interface
+tokenize :: String -> Int -> String -> [Token]
+tokenize fname lineno line = tokenizeImp fname lineno 1 line
