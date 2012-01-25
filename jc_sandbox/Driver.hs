d0ec1fd9dc165f3338b7824a17453b7674919eb2
Jasson Casey <jasson.casey@gmail.com>
2012-01-25 08:26:27 -0600

    Attempting to link input to system chain.

--- jc_sandbox/Driver.hs
+++ jc_sandbox/Driver.hs
@@ -1,4 +1,12 @@
 import System
 import Parser
 
-main = putStrLn "Running ..."
+process :: [String] -> IO ()
+process ( file : files ) = 
+   do buf <- readFile file 
+      x <- parse file buf
+      process files
+process [] = ()
+
+main = do   args <- getArgs
+            process args
--- jc_sandbox/Parser.hs
+++ jc_sandbox/Parser.hs
@@ -8,6 +8,7 @@ parseImp :: [Lexer.Token] -> String
 parseImp (h:tl) = parseImp tl
 parseImp [] = ""
 
+-- parse a filename and string buffer to a list of tokens
 parse :: String -> String -> String
 parse fname buf = 
    let tokens = Lexer.tokenizeBuff fname buf
