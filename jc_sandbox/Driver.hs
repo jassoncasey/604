8ae0082c108e3dfa1d88d83e5f7f3a79493ba4e4
Jasson Casey <jasson.casey@gmail.com>
2012-01-25 09:48:33 -0600

    Some error handling.

--- jc_sandbox/Driver.hs
+++ jc_sandbox/Driver.hs
@@ -1,13 +1,27 @@
 import System
 import Parser
 
+badPostFix name = 
+   show length name < 5 || ( take 4 (reverse name) ) != "lps."
+
+badPostFixes (h:tl) = badPostFix || badPostFixes tl
+badPostFixes [] = False
+
+badUsage args = 
+   show length args < 1 || badPostFixes args
+
 --process :: [String] -> ()
 process ( file : files ) = 
    do buf <- readFile file 
       putStrLn (parse file buf)
       process files
---process [] = ()
+-- what is the proper way to write the terminating portion
+--process [] = () 
 
+-- repeat the processing for each argument
+main :: IO ()
 main = 
    do args <- getArgs
-      process args
+      if badUsage args 
+      then putStrLn "usage error: ./splc <input>.spl+"
+      else process args
