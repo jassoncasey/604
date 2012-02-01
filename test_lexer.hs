1b166ef27841d6cf458963ca53b2b65a31b565d6
Jasson Casey <jasson.casey@gmail.com>
2012-02-01 10:35:02 -0600

    Basic unit test framework for lexer.

--- test_lexer.hs
+++ test_lexer.hs
@@ -4,22 +4,33 @@ import Lexer
 testTokenCls :: [String] -> TokId -> Bool
 testTokenCls (h:tl) symbol =
    let result = tokenizeBuff "" h
-   in if isToken result symbol
+   in if isHeadToken result symbol
       then testTokenCls tl symbol
       else False 
 testTokenCls [] _ = True
 
--- verify a set of predefined tests
+-- runTests :: [( test name, list of tests, expected token id )] -> pass|fail
 runTests :: [(String, [String], TokId)] -> Bool
-runTests ((tests, symbol):tl) =
+runTests ((name, tests, symbol):tl) =
    if testTokenCls tests symbol
       then runTests tl
       else False
 runTests [] = True
 
-tests = [("010123", NatTok)]
+-- To extend an existing class of tests
+-- simpley add another valid input string
+-- to the input list of the proper class below
+digit_tests = [ "0123456789" ]
+id_tests = ["_"]
 
-main :: ()
+-- To extend a class of tests simply add
+-- a new tuple to the list that contains:
+-- name of class of tests, list of valid
+-- strings, and expected lexer token
+tests = [("digit", digit_tests, NatTok),
+         ("identifier", id_tests, IdTok)]
+
+main :: IO ()
 main = do
    if runTests tests
       then putStrLn "Passed lexer tests"
