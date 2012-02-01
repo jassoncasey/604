fe13739bb0bb1b94db7baa7e9351b4d5c6ff9922
Jasson Casey <jasson.casey@gmail.com>
2012-02-01 10:48:54 -0600

    Unit test runner for lexer is in place. To add tests you just specify String input and expected TokId output.

--- test_lexer.hs
+++ test_lexer.hs
@@ -1,27 +1,36 @@
 import Lexer
 
+data Status = Success | Failure
+
 -- verify a list of strings against a token class
-testTokenCls :: [String] -> TokId -> Bool
+testTokenCls :: [String] -> TokId -> ( Status, String )
 testTokenCls (h:tl) symbol =
    let result = tokenizeBuff "" h
    in if isHeadToken result symbol
       then testTokenCls tl symbol
-      else False 
-testTokenCls [] _ = True
+      else ( Failure, "\tFailed subtest: " ++ h ++ "\n" )
+testTokenCls [] _ = ( Success, "" )
+
+-- simple pretty printer for a unit test's status
+getTestStr :: String -> Status -> String -> String
+getTestStr name result msg =
+   case result of 
+      Success -> "Passed" ++ boilerplate
+      Failure -> "Failed" ++ boilerplate
+   where boilerplate = " test: " ++ name ++ "\n" ++ msg
 
 -- runTests :: [( test name, list of tests, expected token id )] -> pass|fail
-runTests :: [(String, [String], TokId)] -> Bool
+runTests :: [(String, [String], TokId)] -> String
 runTests ((name, tests, symbol):tl) =
-   if testTokenCls tests symbol
-      then runTests tl
-      else False
-runTests [] = True
+   (getTestStr name status msg) ++ (runTests tl)
+   where ( status, msg ) = testTokenCls tests symbol
+runTests [] = "Finished ..."
 
 -- To extend an existing class of tests
 -- simpley add another valid input string
 -- to the input list of the proper class below
-digit_tests = [ "0123456789" ]
-id_tests = ["_"]
+digit_tests = [ "0123456789", "a" ]
+id_tests = ["_", "0" ]
 
 -- To extend a class of tests simply add
 -- a new tuple to the list that contains:
@@ -31,7 +40,4 @@ tests = [("digit", digit_tests, NatTok),
          ("identifier", id_tests, IdTok)]
 
 main :: IO ()
-main = do
-   if runTests tests
-      then putStrLn "Passed lexer tests"
-      else putStrLn "Failed lexer tests"
+main = putStrLn ("Starting test runner...\n" ++ (runTests tests))
