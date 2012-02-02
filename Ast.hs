38096326f4aab0fa0a87678d3fde65e14c346652
Jasson Casey <jasson.casey@gmail.com>
2012-02-02 10:32:07 -0600

    Initial Ast structure.

---
+++ Ast.hs
@@ -0,0 +1,21 @@
+module Ast (
+) where
+
+data Type = 
+   Nat
+   | Func Type Type
+   deriving (Show,Eq)
+
+data Operator = Add | Minus | Mult | Div | App deriving(Show,Eq)
+
+data Program = Prog Type [Expression] Int
+
+data Expression = 
+   Id Type String
+   | Num Type Int
+   | Let Type Expression Expression
+   | Lambda Type Expression Expression
+   | Unary Type Operator Expression
+   | Binary Type Operator Expression Expression
+   | Compound Type [Expression]
+   deriving (Show,Eq) 
