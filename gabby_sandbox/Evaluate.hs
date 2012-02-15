aad4c9983f31519ba4b3fd5a410f1ed35cea8d70
Michael L <michael.lopez.332@gmail.com>
2012-02-15 16:44:08 -0600

    Evaluator works for some stuff. Adding context.

--- gabby_sandbox/Evaluate.hs
+++ gabby_sandbox/Evaluate.hs
@@ -1,32 +1,60 @@
-module Evaluate( eval ) where
+module Evaluate( evaluate, evalStep, ValueContext(..) ) where
 
 import Ast
 
+-- This version is flawed. It only works for simple lambdas. What we need to do
+-- is bring back contexts
+
+type ValueContext = [(String,Ast)]
+
+-- Really crappy way to do this
+evaluate :: Ast -> Ast
+evaluate t
+  | isValue t = t
+  | otherwise = evaluate $ evalStep t
 
 
-eval :: Ast -> Ast
+evalStep :: Ast -> Ast
 
 -- Evaluate arithmetic
-eval (Application (Application (Constant (Primitive op val)) ast) ast') =
+evalStep (Application (Application (Constant (Primitive op val)) ast) ast') =
   case (eval_ast,eval_ast') of
     (Constant (IntCst a),Constant (IntCst b)) | elem op ["+","-","*","/"]
       -> deltaRule op a b
     (_,_) -> (Application (Application (Constant (Primitive op val)) eval_ast) eval_ast')
-  where eval_ast = eval ast
-        eval_ast' = eval ast'
+  where eval_ast = evalStep ast
+        eval_ast' = evalStep ast'
 
-eval (Application (Lambda sym term) ast)
-  | isValue ast' = eval (betaReduction sym ast' term)
+evalStep (Application (Lambda sym term) ast)
+  | isValue value = evalStep (betaReduction sym value term)
   | otherwise = error "Free variable found in [ast]"
-  where ast' = eval ast
+  where value = evalStep ast
 
-eval (Let (Unique _) _ ast) = eval ast
+-- should check for a value
+evalStep (Application t1 t2)
+  | canApply t1 && isValue value = evalStep $ apply t1 value
+  | otherwise = Application t1 value
+  where value = evalStep t2
+        
 
-eval (Let (Identifier sym) term ast) =
-  let value = eval term in
-  (betaReduction sym value ast)
+evalStep (Let (Unique _) _ ast) = evalStep ast
 
-eval ast = ast
+evalStep (Let (Identifier sym) term ast) =
+  let value = evalStep term in
+  betaReduction sym value ast
+
+evalStep t = t
+
+-- special function to see if a term can be applied to another
+canApply :: Ast -> Bool
+canApply (Application t1 t2) = canApply t1
+canApply (Lambda _ _) = True
+canApply _ = False
+
+apply :: Ast -> Ast -> Ast
+apply (Application t1 t2) v = Application (apply t1 v) t2
+apply (Lambda sym t) v = betaReduction sym v t
+apply t1 v = Application t1 v
 
 -- Beta reduction
 -- Notes:
@@ -51,8 +79,8 @@ betaReduction sym expr other = other
 -- delta rules for optimized arithmetic
 deltaRule :: String -> Integer -> Integer -> Ast
 deltaRule "+" a b = Constant (IntCst (a + b))
-deltaRule "-" a b = Constant (IntCst (a + b))
-deltaRule "*" a b = Constant (IntCst (a + b))
+deltaRule "-" a b = Constant (IntCst (a - b))
+deltaRule "*" a b = Constant (IntCst (a * b))
 deltaRule "/" a b = Constant (IntCst (quot a b))
 deltaRule op _ _ = error ("Passed unrecognized operation: " ++ op ++ "\n")
 
