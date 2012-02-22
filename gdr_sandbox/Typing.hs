847e894ff6cd822fd2ef245013daf16bb34e3fd4
Michael L <michael.lopez.332@gmail.com>
2012-02-22 17:44:18 -0600

    Type inferencer works.

--- gdr_sandbox/Typing.hs
+++ gdr_sandbox/Typing.hs
@@ -1,12 +1,13 @@
 module Typing where
 
 import qualified Ast
-import qualified Environment
+--import qualified Env
 
 data TypeSymbol =
     Arrow TypeSymbol TypeSymbol
   | List TypeSymbol
   | Natural
+  | Any
   | TypeVar Int
   deriving (Show,Eq)
 
@@ -36,21 +37,20 @@ typifyTree t = let (t',_) = typifyTreeImpl' (t,0) in t'
 typifyTreeImpl :: TypeContext -> Ast.Ast -> Int -> (AstT, Int)
 typifyTreeImpl ctx (Ast.Constant (Ast.IntCst a)) n =
   (Constant Natural (Ast.IntCst a), n)
-typifyTreeImpl ctx (Ast.Constant (Ast.Primitive sym k)) n =
+typifyTreeImpl ctx (Ast.Constant (Ast.Primitive sym k)) n
   | elem sym ["+","-","*","/"] =
       (Constant (Arrow Natural (Arrow Natural Natural)) (Ast.Primitive sym k),n)
   | otherwise = error "Unknown Primitive!"
-typifyTreeImpl typifyTreeImpl' ((Ast.Variable (Identifier sym)), n) =
+typifyTreeImpl ctx (Ast.Variable (Ast.Identifier sym)) n =
   case typeVar of
-    Just a -> ((Variable a name),n)
-    Nothing -> error ("Unbound variable '" ++ sym ++ "'."
+    Just a -> ((Variable a (Ast.Identifier sym)),n)
+    Nothing -> error ("Unbound variable '" ++ sym ++ "'.")
   where typeVar = lookUp ctx sym
 {-typifyTreeImpl ctx (Ast.Lambda sym e) n =
   let
     ctx' = bindVar ctx sym (TypeVar n)
     (et,n') = typifyTreeImpl ctx' e
   in-}
-  
 
 
 typifyTreeImpl' :: (Ast.Ast, Int) -> (AstT, Int)
@@ -88,14 +88,14 @@ bindVar context sym ty = ((Ast.Identifier sym),ty):context
 -- Infertype does not use unify. Instead it applies the rules as needed.
 -- Should probably not do that...
 inferType :: TypeContext -> AstT -> TypeSymbol
-inferType ctx (Application t e1 e2)
+{-inferType ctx (Application t e1 e2)
   | isArrow ty1 = let (Arrow ty11 ty12) = ty1 in
     case ty11 of
       ty2 -> ty12
       _   -> error "Expected, got m!y"
   | otherwise = error "Expected an arrow type."
   where ty1 = inferType ctx e1
-        ty2 = inferType ctx e2
+        ty2 = inferType ctx e2-}
 
 {-inferType ctx (Ast.Lambda sym e)
   let
@@ -110,6 +110,93 @@ inferType ctx (Variable _ (Ast.Identifier sym)) =
     Just ty' -> ty'
     Nothing -> error ("Lookup failure. " ++ sym ++ " is not defined.")
   where ty = lookUp ctx sym
+
+
+-- decltype finds the list straight from the Ast
+-- Assume that the context is populated?
+declType :: TypeContext -> Int -> Ast.Ast -> (Int,TypeSymbol)
+declType ctx n (Ast.Variable (Ast.Identifier sym)) =
+  case ty of
+    Just ty' -> (n,ty')
+    Nothing -> error ("Lookup failure:'" ++ sym ++ "' is not defined.")
+  where ty = lookUp ctx sym
+declType _ n (Ast.Constant (Ast.Primitive sym _))
+  | elem sym ["+","-","*","/"] = (n,(Arrow Natural (Arrow Natural Natural)))
+  | otherwise = error ("Lookup failure:'" ++ sym ++ "' is not defined.")
+
+declType _ n (Ast.Constant (Ast.Constructor "cons" _)) =
+  let t = TypeVar n in
+  (n + 1 ,Arrow t (Arrow (List t) (List t)))
+declType _ n (Ast.Constant (Ast.Constructor "nil" _)) =
+  (n + 1 , List (TypeVar n))
+declType _ _ (Ast.Constant (Ast.Constructor a _)) =
+  error ("Unknown constructor " ++ a ++ ".")
+
+declType _ n (Ast.Constant (Ast.IntCst _)) = (n,Natural)
+
+declType ctx n (Ast.Lambda sym e) =
+  let
+    ctx' = bindVar ctx sym (TypeVar n)
+    (n', te) = declType ctx' (n+1) e
+  in (n', Arrow (TypeVar n) te)
+
+declType ctx n (Ast.Application e1 e2)
+  | isArrow atob = case atob of
+      Arrow a' b -> (n'',subType a' a b)
+      _ -> error ("Expected a, but got _ in _")
+  | otherwise = error ("Expected a -> b, but got _ in _")
+  where (n',atob)  = declType ctx n e1
+        (n'',a) = declType ctx n' e2
+
+-- let statements with unique names are thrown away. Hence, the type result is
+--declType ctx n (Ast.Let (Unique _) e1 e2)
+    
+
+-- This decltype gathers constraints on application
+declType' :: TypeContext -> Int -> Ast.Ast -> (Int,TypeSymbol,ConstraintSet)
+declType' ctx n (Ast.Variable (Ast.Identifier sym)) =
+  case ty of
+    Just ty' -> (n,ty',[])
+    Nothing -> error ("Lookup failure:'" ++ sym ++ "' is not defined.")
+  where ty = lookUp ctx sym
+declType' _ n (Ast.Constant (Ast.Primitive sym _))
+  | elem sym ["+","-","*","/"] = (n,(Arrow Natural (Arrow Natural Natural)),[])
+  | otherwise = error ("Lookup failure:'" ++ sym ++ "' is not defined.")
+
+declType' _ n (Ast.Constant (Ast.Constructor "cons" _)) =
+  let t = TypeVar n in
+  (n + 1 ,Arrow t (Arrow (List t) (List t)),[])
+declType' _ n (Ast.Constant (Ast.Constructor "nil" _)) =
+  (n + 1 , List (TypeVar n),[])
+declType' _ _ (Ast.Constant (Ast.Constructor a _)) =
+  error ("Unknown constructor " ++ a ++ ".")
+
+declType' _ n (Ast.Constant (Ast.IntCst _)) = (n,Natural,[])
+
+declType' ctx n (Ast.Lambda x e) =
+  let
+    ctx' = bindVar ctx x (TypeVar n)
+    (n',te,cs) = declType' ctx' (n+1) e
+    te' = unifyOn cs te
+  in (n',Arrow (TypeVar n) te',cs)
+
+declType' ctx n (Ast.Application e1 e2)
+  | isArrow atob = case atob of
+      Arrow a' b -> (n'',b , ((atob,Arrow a b):(c1++c2)))
+      _ -> error ("Expected a, but got _ in _")
+  | otherwise = error ("Expected a -> b, but got _ in _")
+  where (n',atob,c1)  = declType' ctx n e1
+        (n'',a,c2) = declType' ctx n' e2
+
+declType' ctx n (Ast.Let (Ast.Unique _) e1 e2) =
+  let _ = declType' ctx 0 e1 in declType' ctx n e2
+
+declType' ctx n (Ast.Let (Ast.Identifier sym) e1 e2) =
+  let
+    (n',e1t,cs1) = declType' ctx n e1
+    ctx' = bindVar ctx sym e1t
+    (n'',e2t,cs2) = declType' ctx' n' e2
+  in (n'',unifyOn (cs1++cs2) e2t,cs1++cs2)
 
 
 -- Gathers all constraints for an Ast
@@ -128,7 +215,8 @@ getConstraints (Application t e1 e2) cs =
     constraint = (getType e1, Arrow (getType e2) t)
   in constraint : (cs1++cs2++cs)
 getConstraints (Constant _ _) cs = cs
-getConstraints (Lambda _ _ _) cs = cs
+getConstraints (Variable _ _) cs = cs
+getConstraints (Lambda _ _ e) cs = getConstraints e cs
 
 
 
@@ -148,7 +236,16 @@ unify ((t,u):cs)
         (Arrow t1 t2) = t
         (Arrow u1 u2) = u
       in unify ((t1,u1):(t2,u2):cs)
+  | isList t && isList u =
+      let
+        (List t') = t
+        (List u') = u
+      in unify ((t',u'):cs)
   | otherwise = error "SUUUUUUPP, HOLMES!"
+
+unifyOn :: ConstraintSet -> TypeSymbol -> TypeSymbol
+unifyOn [] t = t
+unifyOn cs t = applySub (unify cs) t
 
 isTypeVar :: TypeSymbol -> Bool
 isTypeVar (TypeVar _) = True
