7eada5d4ea9206a1f49de281b19be508ed2d5342
Jasson Casey <jasson.casey@gmail.com>
2012-02-26 15:17:41 -0600

    Basic instantiate of universal types is now working, with nice printers.

--- gdr_sandbox/ProofTree.hs
+++ gdr_sandbox/ProofTree.hs
@@ -12,19 +12,19 @@ data Type =
    | Forall Int Type
    deriving (Show,Eq)
 
-type Requirements = [ ( Type, Type ) ]
+type Substitution = [ ( Type, Type ) ]
 type Environment  = [ ( Ast.Name, Type ) ]
 
 data Context = 
    Ctx {
       id    :: Int,
-      reqs  :: Requirements,
+      sub   :: Substitution,
       env   :: Environment
    }
 
 data ProofTree = 
    Proof { 
-      ctx   :: Context
+      ctx   :: Context,
       term  :: Ast.Ast,
       type_ :: Type,
       rule  :: String,
@@ -32,8 +32,8 @@ data ProofTree =
    }
 
 -- built in type definitions
-cons' = Forall 1 (Arrow (Alpha 1) (Arrow (List (Alpha 1)) (Alpha 1)))
-nil'  = Forall 1 (Arrow (List (Alpha 1)) (Alpha 1))
+cons' = Forall 1 (Arrow (Alpha 1) (Arrow (List (Alpha 1)) (List (Alpha 1))))
+nil'  = Forall 2 (List (Alpha 2)) 
 binat = Arrow Natural (Arrow Natural Natural)
 
 -- provide a helper for the base environment
@@ -47,23 +47,67 @@ base = [
          (Ast.Identifier "/", binat) 
        ]
 
--- generate a generic instance from a type scheme
-instantiate :: Type -> Int -> [Int] -> ( Type, Int )
-instantiate (Arrow lhs rhs) id bound = 
-   ( Arrow lhs' rhs', idr' )
-   where ( lhs', idl' ) = instantiate lhs id bound
-         ( rhs', idr' ) = instantiate rhs id bound
-instantiate (List type_) id bound =
-   ( List r, id' )
-   where ( r, id' ) = instantiate type_ id bound
-instantiate (Forall arity type_) id bound = 
-   instantiate type_ id (arity:bound)
-instantiate Natural id bound = ( Natural, id )
-instantiate (Alpha id) next_id bound = 
-   if elem id bound 
-      then ( Alpha next_id, next_id+1 )
-      else ( Alpha id, next_id )
+-- helpder to drop type variables bound more than once
+type TypeBinding = ( Int, Int )
+dropBinding :: Int -> [TypeBinding] -> [TypeBinding]
+dropBinding id list =
+   filter dropPredicate list
+   where dropPredicate ( lhs, _ ) = id /= lhs
 
+-- generate a generic instance from a type scheme
+instantiate :: Type -> Int -> [(Int,Int)] -> ( Type, Int )
+instantiate (Arrow lhs rhs) id bindings = 
+   ( Arrow lhs' rhs', idr' )
+   where ( lhs', idl' ) = instantiate lhs id bindings
+         ( rhs', idr' ) = instantiate rhs idl' bindings
+instantiate (List type_) id bindings =
+   ( List r, id' )
+   where ( r, id' ) = instantiate type_ id bindings
+instantiate (Forall id type_) next_id bindings =
+   instantiate type_ (next_id+1) ((id, next_id):bindings')
+   where bindings' = dropBinding id bindings
+instantiate Natural id bindings = ( Natural, id )
+instantiate (Alpha id) next_id bindings = 
+   ( Alpha (lookupBinding id bindings), next_id )
+   where 
+      lookupBinding id ((lhs, rhs):tail) = 
+         if id == lhs 
+            then rhs
+            else lookupBinding id tail
+      lookupBinding id [] = id
+
+-- print the type
+getStrType :: Type -> String
+getStrType (Arrow lhs rhs) =
+   (getStrType lhs) ++ "->" ++ (getStrType rhs)
+getStrType (List type_) = 
+   "[" ++ (getStrType type_) ++ "]"
+getStrType (Forall id type_) = 
+   "forall a_" ++ (show id) ++ "." ++ (getStrType type_)
+getStrType Natural = 
+   "Nat"
+getStrType (Alpha id) = 
+   "a_" ++ (show id)
+
+-- ast.name printer
+getStrName :: Ast.Name -> String
+getStrName (Identifier name) = name
+getStrName (Unique id) = "v_" ++ (show id)
+
+-- environment printer
+getStrEnvironment :: Environment -> String
+getStrEnvironment env =
+   "{ " ++ (makeString env) ++ "}"
+   where 
+      makeString :: Environment -> String
+      makeString ((name, type_):tail) = 
+            if null tail
+               then (getStrName name) ++ ":" ++ (getStrType type_) ++ " " ++
+                  (makeString tail)
+               else (getStrName name) ++ ":" ++ (getStrType type_) ++ ", " ++
+                  (makeString tail)                  
+      makeString [] = ""
+{-
 -- environment loookup
 lookup :: Environment -> Ast.Name -> Maybe Type
 lookup ((key, type_):tl) name =
@@ -117,3 +161,5 @@ proofTree ctx' t@(Lambda param body) =
     Proof{ ctx = ctx', term = t, type_ = type_', rule = "Abs", prem = [] }
 proofTree ctx' t@(Let name input body) = 
     Proof{ ctx = ctx', term = t, type_ = type_', rule = "Let", prem = [] }
+
+-}
